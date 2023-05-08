bbdec8f0bc93e073ba08c7f713a5f7730378e7b8
samuelespina <samuelespina360@gmail.com>
2023-05-08 16:55:40 +0200

    init repo

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav></nav>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useRef } from "react";
+import AOS from "aos";
+import Marquee from "react-fast-marquee";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+
+const HomePage = () => {
+  return <div className="homepage"></div>;
+};
+
+export default HomePage;
